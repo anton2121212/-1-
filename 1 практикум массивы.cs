9b366f4a938cdee8451bf2f01853193e43a91324
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2023-02-27 16:22:22 +0300

    Add files via upload

---
+++ 1 практикум массивы.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//задача 1
+//int[] myArray = new int[5];
+//for (int i = 0; i < myArray.Length; i++)
+//{
+//    Console.WriteLine("Элемент {0}: {1}", i, myArray[i]);
+//}
+
+//задача 2
+
+//int[] myArray = new int[] { 1, 2, 3, 4, 5 };
+
+//задача 3
+
+//int[] myArray = new int[5]; 
+
+//for (int i = 0; i < myArray.Length; i++)
+//{
+//    myArray[i] = i;
+//}
+
+//foreach (int num in myArray)
+//{
+//    Console.Write(num + " ");
+//}
+//
+//задача 4
+
+//int[] myArray = new int[5]; 
+
+//Random random = new Random();
+//for (int i = 0; i < myArray.Length; i++)
+//{
+//    myArray[i] = random.Next(10);
+//}
+
+//foreach (int num in myArray)
+//{
+//    Console.Write(num + " ");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
